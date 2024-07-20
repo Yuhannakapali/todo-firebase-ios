3ddace9e993b5d24d271f8dcd8825553926251d0
Yuhanna Kapali <yuhannakapali@gmail.com>
2024-07-21 01:31:47 +0545

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by yuhanna kapali on 21/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
