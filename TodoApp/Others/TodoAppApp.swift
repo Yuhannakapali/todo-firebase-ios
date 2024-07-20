20c6adc42e44e3754ee6a66afc38701d479f644e
Yuhanna Kapali <yuhannakapali@gmail.com>
2024-07-21 02:06:02 +0545

    implimented the file structure and imported firebase configuration file

--- TodoApp/Others/TodoAppApp.swift
+++ TodoApp/Others/TodoAppApp.swift
@@ -4,11 +4,14 @@
 //
 //  Created by yuhanna kapali on 21/07/2024.
 //
-
+import FirebaseCore
 import SwiftUI
 
 @main
 struct TodoAppApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
