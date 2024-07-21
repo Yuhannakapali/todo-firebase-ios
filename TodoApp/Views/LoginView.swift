5aa4d4e2f862f7893ff91bb016cc8997de0edf3f
Yuhanna Kapali <yuhannakapali@gmail.com>
2024-07-21 16:22:20 +0545

    renaming the views files

--- TodoApp/Views/LoginView.swift
+++ TodoApp/Views/LoginView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct Login: View {
+struct LoginView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    Login()
+    LoginView()
 }
--- TodoApp/Views/NewTodoView.swift
+++ TodoApp/Views/NewTodoView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct NewTodo: View {
+struct NewTodoView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    NewTodo()
+    NewTodoView()
 }
